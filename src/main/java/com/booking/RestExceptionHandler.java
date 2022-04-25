2f95fe25507be5f16b9dece1f2433721a4ccfda8
Rostyslav Balushchak <meta-x-one@meta.ua>
2022-04-25 17:35:41 +0300

    Initial commit

---
+++ src/main/java/com/booking/RestExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.booking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(RestExceptionHandler.class)
+public class RestExceptionHandler {
+
+  @Autowired
+  RestExceptionHandler restExceptionHandler;
+
+}
